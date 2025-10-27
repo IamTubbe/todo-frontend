b89e0cbdb253197fa7f1d3b60bb62d4311f9a397
IamTubbe <67030298@kmitl.ac.th>
2025-10-28 04:16:21 +0700

    fix: enable basePath and assetPrefix for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
-// const repoName = 'todo-frontend'; // <--- คอมเมนต์ไว้
+const repoName = 'todo-frontend'; // <--- เอา // ออก
 const nextConfig = {
   output: 'export',
-  // basePath: `/${repoName}`,     // <--- คอมเมนต์ไว้
-  // assetPrefix: `/${repoName}/`, // <--- คอมเมนต์ไว้
+  basePath: `/${repoName}`,     // <--- เอา // ออก
+  assetPrefix: `/${repoName}/`, // <--- เอา // ออก
   images: {
     unoptimized: true,
   },
